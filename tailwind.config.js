bd36f074b43c01480ba2a229b46789958a67df56
RITIKA MISHRA <i.ritikamish@gmail.com>
2024-06-09 17:41:12 +0530

    Section-5 added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        '95': '95px',
+        '152' : '152px',
+      },
+    },
   },
   plugins: [],
 }
